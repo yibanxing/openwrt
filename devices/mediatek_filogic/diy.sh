0278f2adf642abdd884e26dd2a9cb79502281008
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-05-24 21:20:06 +0800

    update

--- devices/mediatek_filogic/diy.sh
+++ devices/mediatek_filogic/diy.sh
@@ -3,8 +3,3 @@
 shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-
-rm -rf tools/mkimage
-svn co https://github.com/openwrt/openwrt/trunk/tools/mkimage tools/mkimage
-
-curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/master/scripts/mkits.sh -o scripts/mkits.sh
